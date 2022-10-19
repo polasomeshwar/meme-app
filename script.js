838b4568a6bdcb7a7a2e2e19dc4c36d84a711303
Pola Someshwar <57764440+polasomeshwar@users.noreply.github.com>
2022-10-19 21:21:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const API = "https://meme-api.herokuapp.com/gimme";
+const getImageUrl = async()=>{
+    let resp = await fetch(API);
+    return resp.json();
+}
+const fetchNext = async()=>{
+    let resp = await fetch(API);
+    let image = document.querySelector("#memeId");
+    let {url} = await resp.json(); 
+    image.src = url;
+};
+document.addEventListener("DOMContentLoaded",async()=>{
+   console.log("Hello world");
+   let image = document.querySelector("#memeId");
+   let memeDetails = await getImageUrl();
+   console.log(memeDetails); 
+   let {url} = memeDetails;
+   image.src = url;
+   document.querySelector("#nextBtn").addEventListener("click",fetchNext);
+}); 
